d2b053ef01608e3de8959bc40e966a5b2ab19c0c
Ivan020709 <qkrwjdghks020709@gmail.com>
2026-09-11 10:01:16 +0900

    yaml 수정 키 변경 완료, 카카오인포 아이피 변경

--- src/main/java/com/fastcam/springserver/controller/MemberController.java
+++ src/main/java/com/fastcam/springserver/controller/MemberController.java
@@ -206,7 +206,7 @@ public class MemberController {
             ms.insertKakaoMember(mdto);
             mdto = ms.getMemberBySnsid( kakaoProfile.getId() );
         }
-        response.sendRedirect("http://54.180.155.24:3000/savekakaoinfo/" + mdto.getUserid());
+        response.sendRedirect("http://3.35.4.91/savekakaoinfo/" + mdto.getUserid());
 
     }
 
